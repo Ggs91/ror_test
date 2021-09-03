b1e8340d67f15da2331376c974c61c84f61c7774
Ggs91 <georges_atalla@hotmail.fr>
2021-09-03 20:41:55 +0200

    content attribute presence spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  it { should belong_to(:user) }
-  it { should belong_to(:post) }
+  context 'associations' do
+    it { should belong_to(:user) }
+  end
+
+  context 'validations' do
+    it { should validate_presence_of(:content).with_message('Content must be present') }
+  end
 end
