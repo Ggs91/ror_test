5a8b5986cb4b1429595e023a4b7ed6d6d6d5fa98
Ggs91 <georges_atalla@hotmail.fr>
2021-09-03 21:17:57 +0200

    add likes association

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :posts
+  has_many :likes, as: :likeable, dependent: :destroy
 
   validates_presence_of :content, message: "Content must be present"
 end
