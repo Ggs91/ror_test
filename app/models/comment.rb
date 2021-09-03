ed0133ef49bbb4d968ca227fa45061e2bd596cb7
Ggs91 <georges_atalla@hotmail.fr>
2021-09-03 21:32:12 +0200

    correct typo

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
-  belongs_to :posts
+  belongs_to :post
   has_many :likes, as: :likeable, dependent: :destroy
 
   validates_presence_of :content, message: "Content must be present"
