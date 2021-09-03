847351a8720b230eccd30c74295b134a8b4ebfa7
Ggs91 <georges_atalla@hotmail.fr>
2021-09-03 21:18:28 +0200

    add likes association spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   context 'associations' do
     it { should belong_to(:user) }
+    it { should have_many(:likes) }
   end
 
   context 'validations' do
