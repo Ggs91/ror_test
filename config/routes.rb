cf518004b7244c3c23c3423b371d387eb679d100
Ggs91 <georges_atalla@hotmail.fr>
2021-09-03 19:19:39 +0200

    add comments routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1, defaults: { format: :json } do
       resources :users, only: [:index]
-      resources :posts, only: [:index]
+      resources :posts, only: [:index] do
+        resources :comments, only: [:show, :index, :create]
+      end
     end
   end
 end
