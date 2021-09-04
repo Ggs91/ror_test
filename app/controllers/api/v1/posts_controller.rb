4e0c91cf1ae08919bf0c1b39a3f115c132625acc
Ggs91 <georges_atalla@hotmail.fr>
2021-09-04 18:38:30 +0200

    inherit from BaseController

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::PostsController < ActionController::API
+class Api::V1::PostsController < Api::V1::BaseController
   def index
     posts = Post.all
     render json: posts, each_serializer: Api::PostSerializer
