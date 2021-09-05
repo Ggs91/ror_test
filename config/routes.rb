e0c6c512d0e20566dd37844ae54f90230b461e90
Ggs91 <georges_atalla@hotmail.fr>
2021-09-06 00:40:24 +0200

    add likes resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
     namespace :v1, defaults: { format: :json } do
       resources :users, only: [:index]
       resources :posts, only: [:index] do
-        resources :comments, only: [:show, :index, :create]
+        resources :comments, only: [:index, :create]
+        resources :likes, only: [:create], module: :posts
       end
       match '*path', to: 'base#render_not_found', via: [:get, :post]
     end
