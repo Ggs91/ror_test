94423f96e4d25ee52f315908d88b563c5ee38254
Ggs91 <georges_atalla@hotmail.fr>
2021-09-06 00:38:13 +0200

    abstract default namespace folder for serializers

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::BaseController < ApplicationController
   before_action :set_default_format
   before_action :require_current_user
+  before_action do
+    self.namespace_for_serializer = Api::V1
+  end
 
   def render_not_found
     render_errors([{ code: '2', message: 'Resource not found' }], :not_found)
