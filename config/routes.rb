308fa372107dc97ed46844a131100d3d1e2a9286
Ggs91 <georges_atalla@hotmail.fr>
2021-09-06 02:24:29 +0200

    add likes routes to comments resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
     namespace :v1, defaults: { format: :json } do
       resources :users, only: [:index]
       resources :posts, only: [:index] do
-        resources :comments, only: [:index, :create]
         resources :likes, only: [:create], module: :posts
+        resources :comments, only: [:index, :create] do
+          resources :likes, only: [:create], module: :comments
+        end
       end
       match '*path', to: 'base#render_not_found', via: [:get, :post]
     end
