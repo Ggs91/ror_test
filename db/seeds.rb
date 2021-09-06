575221c5f1a33df448d52c75f8c1a13ee69751e1
Ggs91 <georges_atalla@hotmail.fr>
2021-09-06 22:42:20 +0200

    create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Like.destroy_all
+Comment.destroy_all
+Post.destroy_all
+User.destroy_all
+
+Like.reset_pk_sequence
+Comment.reset_pk_sequence
+Post.reset_pk_sequence
+User.reset_pk_sequence
+
+u1 = User.create!
+
+50.times do
+  User.create!
+end
+
+p1 = Post.create!(
+  user: u1,
+  description: "My first post !"
+)
+
+100.times do |i|
+  Comment.create!(
+    user: User.find(rand(1..50)),
+    post: p1,
+    content: "Great post !"
+  )
+end
+
+Comment.all.each do |comment|
+  5.times do |i|
+    Like.create!(
+      user: User.find(rand(1..50)),
+      likeable: comment
+    )
+  end
+end
+
+puts "-----#{User.all.size} Users created-----"
+puts "-----#{Post.all.size} Posts created-----"
+puts "-----#{Comment.all.size} Comments created-----"
+puts "-----#{Like.all.size} Likes created-----"
