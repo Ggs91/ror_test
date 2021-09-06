1d97246b80a20600b587024d89d0d43180c69415
Ggs91 <georges_atalla@hotmail.fr>
2021-09-06 16:01:30 +0200

    implement #total_likes

--- app/serializers/api/v1/comment_serializer.rb
+++ app/serializers/api/v1/comment_serializer.rb
@@ -1,12 +1,16 @@
 module Api
   module V1
     class CommentSerializer < Api::V1::BaseSerializer
-      attributes :id, :content, :liked
+      attributes :id, :content, :liked, :total_likes
       belongs_to :user
       belongs_to :post
 
       def liked
         object.likes.any? { |like| like.user_id == scope.id }
+      end
+
+      def total_likes
+        object.likes_count
       end
     end
   end
