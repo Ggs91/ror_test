79fad7545e4bb146af55ef8c4e0967e99aa9ed59
Ggs91 <georges_atalla@hotmail.fr>
2021-09-06 02:23:50 +0200

    add 'liked' field

--- app/serializers/api/v1/comment_serializer.rb
+++ app/serializers/api/v1/comment_serializer.rb
@@ -1,9 +1,13 @@
 module Api
   module V1
     class CommentSerializer < Api::V1::BaseSerializer
-      attributes :id, :content
+      attributes :id, :content, :liked
       belongs_to :user
       belongs_to :post
+
+      def liked
+        object.likes.any? { |like| like.user_id == scope.id }
+      end
     end
   end
 end
