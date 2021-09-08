2c423455a0bea60f2f8b35209e89c341009c8492
Ggs91 <georges_atalla@hotmail.fr>
2021-09-08 15:05:20 +0200

    add belongs_to user attribute

--- app/serializers/api/v1/like_serializer.rb
+++ app/serializers/api/v1/like_serializer.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     class LikeSerializer < Api::V1::BaseSerializer
       attributes :id
+      belongs_to :user
       belongs_to :likeable, polymorphic: true
     end
   end
