6c27fcac7e28fe6d8c592693fbd37505c43b3a25
Ggs91 <georges_atalla@hotmail.fr>
2021-09-08 15:09:34 +0200

    include ExceptionHandler module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::API
   include Pagination
+  include Error::ExceptionsHandler
 
+  before_action :require_current_user
   serialization_scope :current_user
 
   # To simplify the test, I'm using the first user in database
   def current_user
     User.first
+  end
+
+  private
+
+  def require_current_user
+    return if current_user
+    render json: { error: 'you must be logged in' }, status: :unauthorized
   end
 end
