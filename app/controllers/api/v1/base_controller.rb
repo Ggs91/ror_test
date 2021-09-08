8a82a1fba7c9b2970c2982fcd63ab55bdeb7898f
Ggs91 <georges_atalla@hotmail.fr>
2021-09-08 15:16:34 +0200

    remove comments

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,9 +1,6 @@
 class Api::V1::BaseController < ApplicationController
   before_action :set_default_format
   before_action :set_serializer_namespace
-  # before_action do
-  #   self.namespace_for_serializer = Api::V1
-  # end
 
   private
 
