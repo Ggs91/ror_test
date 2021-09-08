a2ea38aacca166bf5cbe19e80156b1cde814d885
Ggs91 <georges_atalla@hotmail.fr>
2021-09-08 15:04:49 +0200

    move code to application_controller

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,29 +1,17 @@
 class Api::V1::BaseController < ApplicationController
   before_action :set_default_format
-  before_action :require_current_user
-  before_action do
-    self.namespace_for_serializer = Api::V1
-  end
-
-  def render_not_found
-    render_errors([{ code: '2', message: 'Resource not found' }], :not_found)
-  end
+  before_action :set_serializer_namespace
+  # before_action do
+  #   self.namespace_for_serializer = Api::V1
+  # end
 
   private
-
-  def require_current_user
-    if current_user
-      return
-    end
-
-    render_errors([{ code: '1', message: 'you must be logged in' }], :unauthorized)
-  end
 
   def set_default_format
     request.format = :json
   end
 
-  def render_errors(errors=[], status=:ok)
-    render json: { status: :error, errors: errors }, status: status
+  def set_serializer_namespace
+    self.namespace_for_serializer = Api::V1
   end
 end
