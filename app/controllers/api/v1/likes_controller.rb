7607f4359d7fb81ed61e24f210441db16de6f6ae
Ggs91 <georges_atalla@hotmail.fr>
2021-09-09 01:04:52 +0200

    create StatusCreatedSerializer

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -3,6 +3,6 @@ class Api::V1::LikesController < Api::V1::BaseController
   def create
     like = @likeable.likes.create!(user: current_user)
 
-    render json: like, include: ['user', 'likeable']
+    render json: like, serializer: Api::V1::StatusCreatedSerializer, status: :created
   end
 end
