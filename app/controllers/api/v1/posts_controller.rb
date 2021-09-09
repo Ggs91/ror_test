155dcef6eecbe81ce3a00509b0805e37380b1112
Ggs91 <georges_atalla@hotmail.fr>
2021-09-10 00:57:25 +0200

    add pagination to #index

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::PostsController < Api::V1::BaseController
   def index
-    posts = Post.all
+    posts = Post.all.page(page).per(per_page)
+
     render json: posts
   end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::UsersController < Api::V1::BaseController
   def index
-    users = User.all
+    users = User.all.page(page).per(per_page)
+
     render json: users
   end
 end
