457a49376baf1a129cd7e8615e5632537310fa5b
Ggs91 <georges_atalla@hotmail.fr>
2021-09-10 01:51:43 +0200

    remove useless comments

--- spec/requests/api/v1/comments_request_spec.rb
+++ spec/requests/api/v1/comments_request_spec.rb
@@ -1,15 +1,5 @@
-RSpec.describe 'Posts API endpoint', type: :request do
+require 'rails_helper'
+
+RSpec.describe 'Comments API endpoint', type: :request do
 
 end
-
-
-      #         context 'when unsuccessful response' do
-      #   context 'when record invalid' do
-      #     describe 'HTTP response status-code' do
-
-      #     end
-      #     describe 'response body' do
-
-      #     end
-      #   end
-      # end
