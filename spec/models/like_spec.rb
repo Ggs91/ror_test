7274c19d4f976603c6057943d832021754bcc1cc
Ggs91 <georges_atalla@hotmail.fr>
2021-09-10 00:58:18 +0200

    add spec for counter_cache

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe Like, type: :model do
   context 'associations' do
     it { should belong_to(:user) }
-    it { should belong_to(:likeable) }
+    it { should belong_to(:likeable).counter_cache(:likes_count) }
   end
 end
