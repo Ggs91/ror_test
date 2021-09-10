41a1082ea4e8496cce55f539505a1fccff45f036
Ggs91 <georges_atalla@hotmail.fr>
2021-09-10 11:44:44 +0200

    add likes_count to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_06_103647) do
+ActiveRecord::Schema.define(version: 2021_09_10_094246) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2021_09_06_103647) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "likes_count", default: 0, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
