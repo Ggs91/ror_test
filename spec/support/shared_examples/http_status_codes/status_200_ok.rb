64527c2912fb4269de7006a26b0746309ecd12fa
Ggs91 <georges_atalla@hotmail.fr>
2021-09-10 15:17:51 +0200

    refactor status ok shared example

--- spec/support/shared_examples/http_status_codes/status_200_ok.rb
+++ spec/support/shared_examples/http_status_codes/status_200_ok.rb
@@ -1,5 +1,7 @@
 RSpec.shared_examples 'a 200 ok status' do
-  it 'respond with 200 ok' do
-    expect(response).to have_http_status(:ok)
+  describe 'HTTP response status-code' do
+    it 'respond with 200 ok' do
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
