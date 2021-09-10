ea3171944a4df116da74f9e4f52150f160de6925
Ggs91 <georges_atalla@hotmail.fr>
2021-09-11 01:20:34 +0200

    load object creator

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,7 @@ require 'support/request_helpers'
 Dir["./spec/support/**/*.rb"].sort.each { |f| require f}
 RSpec.configure do |config|
   config.include Requests::JsonHelpers, type: :request
+  config.include Requests::ObjectsCreator, type: :request
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
