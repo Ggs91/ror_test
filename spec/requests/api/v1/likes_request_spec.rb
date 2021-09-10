0f30ed1247cb4fe8baf95adaa4ce759ad4247cf1
Ggs91 <georges_atalla@hotmail.fr>
2021-09-10 11:51:30 +0200

    implement likes API

--- spec/requests/api/v1/likes_request_spec.rb
+++ spec/requests/api/v1/likes_request_spec.rb
@@ -3,15 +3,22 @@ require 'rails_helper'
 RSpec.describe 'Likes API endpoint', type: :request do
   context 'POST' do
     describe "#create" do
+      before do
+        post = create(:post)
+        post api_v1_post_likes_path(post), params: { post: post }
+      end
+
       context 'when post resource' do
-        let(:resource) { create(:post) }
+
+        it_behaves_like 'a 201 created status'
+
         it 'creates a like for a post' do
+        end
+
+        it 'responds with the created resource' do
 
         end
       end
-      # describe 'HTTP response status-code' do
-      #   it_behaves_like 'a 201 created status'
-      # end
     end
   end
 end
