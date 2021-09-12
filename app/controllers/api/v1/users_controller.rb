40d5689d8a75f373e11a16988af1af4a134332ec
Ggs91 <georges_atalla@hotmail.fr>
2021-09-12 23:55:09 +0200

    includes user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < Api::V1::BaseController
   def index
-    users = User.all.page(page).per(per_page)
+    users = User.page(page).per(per_page)
 
     render json: users
   end
