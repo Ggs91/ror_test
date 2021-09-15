c4abae383b19d9c8d82165902a012b54342069bf
Ggs91 <georges_atalla@hotmail.fr>
2021-09-15 19:10:59 +0200

    change syntaxe

--- spec/support/shared_examples/pagination_shared_example.rb
+++ spec/support/shared_examples/pagination_shared_example.rb
@@ -2,13 +2,15 @@ RSpec.shared_examples 'a paginable resource' do
 
   subject { response_body['data'].count }
 
-  it 'when page=1&per_page=2' do
-    get path, params: { page: 1, per_page: 2 }
-    expect(subject).to eq(2)
+  context 'when page=1&per_page=2' do
+    before { get path, params: { page: 1, per_page: 2 } }
+
+    it { is_expected.to eq(2) }
   end
 
-  it 'when page=1&per_page=3' do
-    get path, params: { page: 1, per_page: 3 }
-    expect(subject).to eq(3)
+  context 'when page=1&per_page=3' do
+    before { get path, params: { page: 1, per_page: 3 } }
+
+    it { is_expected.to eq(3) }
   end
 end
