a61528272728037d555e77738a84c15fbb3c176a
Ggs91 <georges_atalla@hotmail.fr>
2021-09-15 19:10:50 +0200

    implement current_user

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -2,6 +2,11 @@ class Api::V1::BaseController < ApplicationController
   before_action :set_default_format
   before_action :set_serializer_namespace
 
+  # To simplify the test, I'm using the first user in database
+  def current_user
+    User.first
+  end
+
   private
 
   def set_default_format
