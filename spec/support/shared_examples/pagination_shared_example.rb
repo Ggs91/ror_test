c9f4ec24ef52210a95dbccc8672e43723deedc7e
Ggs91 <georges_atalla@hotmail.fr>
2021-09-17 21:41:39 +0200

    add links keys specs

--- spec/support/shared_examples/pagination_shared_example.rb
+++ spec/support/shared_examples/pagination_shared_example.rb
@@ -1,16 +1,24 @@
 RSpec.shared_examples 'a paginable resource' do
 
-  subject { response_body['data'].count }
+  describe 'numbre of requested objects' do
+    subject { response_body['data'].count }
 
-  context 'when page=1&per_page=2' do
-    before { get path, params: { page: 1, per_page: 2 } }
+    context 'when page=1&per_page=2' do
+      before { get path, params: { page: 1, per_page: 2 } }
 
-    it { is_expected.to eq(2) }
+      it { is_expected.to eq(2) }
+    end
+
+    context 'when page=1&per_page=3' do
+      before { get path, params: { page: 1, per_page: 3 } }
+
+      it { is_expected.to eq(3) }
+    end
   end
 
-  context 'when page=1&per_page=3' do
-    before { get path, params: { page: 1, per_page: 3 } }
-
-    it { is_expected.to eq(3) }
+  describe 'links keys' do
+    it 'has json api keys' do
+      expect(response_body['links']).to include('first', 'prev', 'next', 'last')
+    end
   end
 end
